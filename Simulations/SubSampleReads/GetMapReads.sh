3f98c405e3ec35eb077ee032ae7b2aa48780e30c
Adrien Oliva <adrioliva@hotmail.fr>
2020-12-16 01:32:19 +1030

    Update GetMapReads.sh

--- Simulations/SubSampleReads/GetMapReads.sh
+++ Simulations/SubSampleReads/GetMapReads.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in *.sam #was working with sam files at the time
+for i in *.sam #change if you work with bamfiles
 do
 samtools view -F 4 $i > $i.map
 sort -u -t$'\t' -k1,1 $i.map > $i.map.unique
